253840d51e22a6c3ba78c589dc848813eb62436f
Myriam Ibe <Myriam@iMac-de-Mahamoud.local>
2022-07-25 01:15:32 +0200

    Design using tailwind css - add of date and time - you can now save your to do list using the local storage - you can erase your tasks once they are done

--- script.js
+++ script.js
@@ -1,13 +1,75 @@
 "use strict"
+var setdate=document.querySelector(".set_date");
+var settime=document.querySelector(".set_time");
+
+var date=new Date().toDateString();
+setdate.innerHTML=date;
+
+
+setInterval(function(){
+    var time=new Date().toLocaleTimeString();
+    settime.innerHTML=time;
+},500);  
+
+
+ol.innerHTML = localStorage.getItem("list")
+
+const spanDels = document.querySelectorAll(".delete");
+
+for (let span of spanDels) {
+
+    span.onclick = () => del(span.parentElement)
+}
+
+    noTask.style.display = (ol.innerHTML == "")? "block" : "none";
+
 
 form.onsubmit = () =>{
+    // Création d'une liste à chaque ajout
     const li = document.createElement("li");
-    li.textContent = field.value;
 
+    // Création d'une croix pour supprimer une tâche
+    const spanDel = document.createElement("span");
+    spanDel.classList.add("delete")
+
+
+    spanDel.textContent = " ❌ "
+    spanDel.style.fontSize = "8px"
+    spanDel.style.marginLeft = '12px'
+    spanDel.style.verticalAlign ="middle"
+    spanDel.onclick = () => del(li);
+
+    li.innerHTML = field.value
+
+
+    li.appendChild(spanDel)
     ol.appendChild(li);
 
     field.value = ""
+    noTask.style.display = "none" ;
+
+// Permet de sauvegarder les infos dans le localStorage
+    localStorage.setItem("list", ol.innerHTML)
 
     return false;
 
+}
+
+// Fonction qui permet de supprimer une tache
+function del(e) {
+    e.remove()
+
+// Affichage de "No task yet" si aucune tâche n'est dans la liste
+    noTask.style.display = (ol.innerHTML == "")? "block" : "none";
+
+    localStorage.setItem("list", ol.innerHTML)
+
+}
+
+
+function checked(){
+    var checked_green=document.getElementById("check");
+    checked_green.classList.toggle('green');
+    var strike_unstrike=document.getElementById("strike");
+    strike_unstrike.classList.toggle('strike_none');
 }
